9b1ff741b5ad73f05b793d7ab46744982bc1bc8f
LeahIsrael26 <76888229+LeahIsrael26@users.noreply.github.com>
2021-05-13 13:17:43 +0300

    Login + Spotify

---
+++ src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+
+//https://accounts.spotify.com/authorize?client_id=5fe01282e94241328a84e7c5cc169164&redirect_uri=http:%2F%2Fexample.com%2Fcallback&scope=user-read-private%20user-read-email&response_type=token&state=123
+
+function Login() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Login;
