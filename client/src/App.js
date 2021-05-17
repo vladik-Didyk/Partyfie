1e7a1d4c7c499ec0767169160832918823135248
Renato <renato.szk@hotmail.com>
2021-05-17 15:51:19 +0300

    fixing_yarn_problem

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,11 @@ import localforage from "localforage";
 import './App.css';
 // import LogInPage from './Components/LogInPage/LogInPage'
 import NavBar from "./NavBar/NavBar"
-import Container from "./Components/Container";
 import HomePage from "./Components/HomePage/HomePage";
 //search process
 // import Search from "./Components/SearchPage/Search";
-import Dashboard from "./Components/SearchPage/Dashboard"
-import Login from "./Components/SearchPage/Login"
+// import Dashboard from "./Components/SearchPage/Dashboard"
+// import Login from "./Components/SearchPage/Login"
 
 
 //https://accounts.spotify.com/authorize?client_id=5fe01282e94241328a84e7c5cc169164&redirect_uri=http:%2F%2Fexample.com%2Fcallback&scope=user-read-private%20user-read-email&response_type=token&state=123
@@ -67,15 +66,15 @@ function App() {
   return (
     <>
     <div className ="login_form">
-      <Container/>
+
    
      <button  className='loginToSpotifyAccount' 
               onClick={handleLogin}
               > Login to Spotify 
       </button>
-      <Dashboard/>
-      <Login/>
-     {code ? <Dashboard code={code} /> : <Login />}
+      {/* <Dashboard/> */}
+      {/* <Login/>
+     {code ? <Dashboard code={code} /> : <Login />} */}
      <HomePage />
     </div>
     </>
