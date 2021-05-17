cacf1c49073b19365428d8a0581822c9a3dc9e2a
itailevi98 <you@example.com>
2021-05-17 18:05:48 +0300

    Merged with Renato's front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,73 +1,24 @@
 import React from "react";
-import { useEffect } from "react";
 import {
     BrowserRouter as Router,
-    Redirect,
     Route,
     Switch
 } from "react-router-dom";
 import "./App.css";
-import AuthProvider, { useAuth } from "./Components/Auth";
+import AuthProvider from "./Components/Auth";
 import SessionPage from "./Components/SessionPage";
 import './App.css';
-
-const CLIENT_ID = "0a933f7d91e64b9096efbc218edaa4cc";
-const REDIRECT_URL_AFTER_LOGIN = "http://localhost:3000";
-const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
-
-const SCOPES = ["user-read-currently-playing"];
-const SPACE_DELIMITER = "%20";
-const SCOPES_URL_PARAMS = SCOPES.join(SPACE_DELIMITER);
-
-const getReturnedParamsFromSpotifyAuth = (hash) => {
-    const stringAfterHash = hash.substring(1);
-    const paramsInUrl = stringAfterHash.split("&");
-    const paramsSplitUp = paramsInUrl.reduce((accumulater, currentValue) => {
-        const [key, value] = currentValue.split("=");
-        accumulater[key] = value;
-        return accumulater;
-    }, {});
-
-    return paramsSplitUp;
-};
-
-function Homepage() {
-    const { token, saveToken } = useAuth();
-    console.log(token);
-    console.log(saveToken);
-
-    useEffect(() => {
-        async function getToken() {
-            if (window.location.hash) {
-                const { access_token } = getReturnedParamsFromSpotifyAuth(window.location.hash);
-                await saveToken(access_token);
-            }
-        }
-        getToken();
-    });
-
-    const handleLogin = () => {
-        window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL_AFTER_LOGIN}&scope=${SCOPES_URL_PARAMS}&response_type=token&show_dialog=true`;
-    };
-
-    return (
-        <div className="login_form">
-            <h1>hi</h1>
-            <button className="loginToSpotifyAccount" onClick={handleLogin}>
-                Login to Spotify
-            </button>
-        </div>
-    );
-}
+import HomePage from "./Components/HomePage/HomePage";
+import NavBar from "./Components/NavBar/NavBar";
 
 function AppRouter() {
-    const { token } = useAuth();
+
     return (
         <Router>
+            <NavBar />
             <Switch>
                 <Route exact path="/">
-                    {token && <Redirect exact to="/session"/>}
-                    {!token && <Homepage />}
+                    <HomePage />
                 </Route>
                 <Route exact path="/session">
                     <SessionPage />
@@ -78,7 +29,6 @@ function AppRouter() {
 }
 
 function App() {
-    
     return (
         <AuthProvider>
             <AppRouter />
---
+++ client/src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,44 @@
+
+import React from "react"
+import css from './NavBar.module.css'
+import {
+    Link,
+  } from "react-router-dom";
+
+
+
+const NavBar = props => {
+
+    return (
+
+      <div className={css.Navbar}>
+        <Link className={css.navitem} 
+              to="/">
+          Home
+        </Link>
+        <Link className={css.navitem} 
+              to="/new_session">
+            Create a Session
+        </Link>
+        <Link className={css.navitem} 
+              to="/join_session">
+          Join a session
+        </Link>
+         
+      <div className={css.LogInAndSignIn}>
+      <Link className={css.navitem} 
+              to="/profile">
+          My Profile
+        </Link>
+        &nbsp;&nbsp;
+        <Link className={css.navitem} 
+              to="/logout">
+          Log Out
+        </Link>
+                    
+                    
+      </div>
+      </div>
+    );
+  };
+  export default NavBar 
